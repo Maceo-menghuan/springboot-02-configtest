000d52067d3ec3f192db1ca33c5ff9c6094fcf34
e <wh_yinhanwei@yunlizhihui.com>
2020-08-06 15:27:48 +0800

    springboot-02config讲解第一次提交

---
+++ src/main/java/com/yhw/springboot02config/Springboot02ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.yhw.springboot02config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinhanwei
+ */
+@SpringBootApplication
+public class Springboot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02ConfigApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yhw/springboot02config/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.yhw.springboot02config.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wh_yi
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello!";
+    }
+}
